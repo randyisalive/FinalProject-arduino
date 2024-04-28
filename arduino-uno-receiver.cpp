9b0099870d1b9f7cc2dd5c70766b968bd08c5aa8
randyisalive <99945506+randyisalive@users.noreply.github.com>
2024-04-28 21:46:13 +0700

    comment

--- arduino-uno-receiver.cpp
+++ arduino-uno-receiver.cpp
@@ -1,36 +1,39 @@
 #include <SoftwareSerial.h>
 #include <LiquidCrystal_I2C.h>
-#define buzzerPin 3
+#define buzzerPin 3 // PIN 3 BUZZER
 
-SoftwareSerial BTSerial(11, 10); // RX | TX
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+SoftwareSerial BTSerial(11, 10);    // RX | TX
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD Display with 0x27 Address, 16x2 pixel
 
 void setup()
 {
 
     // pin mode
-    pinMode(buzzerPin, OUTPUT);
+    pinMode(buzzerPin, OUTPUT); // set the pin to output
 
-    lcd.clear();
+    lcd.clear(); // clear the LCD Display
     Serial.begin(9600);
-    BTSerial.begin(38400); // HC-05 default speed in AT command mode
-    Serial.println("Slave Bluetooth Device is Ready to Pair");
-    delay(3000);
-    lcd.init();
-    lcd.backlight();
+    BTSerial.begin(38400);                                     // HC-05 default speed in AT command mode
+    Serial.println("Slave Bluetooth Device is Ready to Pair"); // tag as slave
+    lcd.init();                                                // initialize the LCD
+    lcd.backlight();                                           // turn on LCD backlight
     lcd.setCursor(0, 0);
-    lcd.print("Hello!");
-    delay(2000);
-    lcd.clear();
+    lcd.print("Hello!"); // Print hello! to greet the user
+    delay(3000);         // wait for 3 seconds
+    lcd.clear();         // clear the LCD
 }
 
 void loop()
 {
     float temp;
-    float tempLimit = 28;
+    float tempLimit = 28.50; // set the temp limit threshold
+
+    // check if BTSerial have input (from transmitter)
     if (BTSerial.available())
-    { // If the data is coming from Software Serial Port
-        String received = BTSerial.readStringUntil('\n');
+    {                                                     // If the data is coming from Software Serial Port
+        String received = BTSerial.readStringUntil('\n'); // read String receive from BTSerial
+
+        // display temperature data to LCD
         lcd.setCursor(0, 0);
         lcd.print("Temp: ");
         received.remove(received.length() - 1); // remove last string (bug)
@@ -38,19 +41,24 @@ void loop()
         lcd.print((char)223);     // the little square
         lcd.print("C");           // Celcius
         Serial.println(received); // Print received message to Serial Monitor
+
+        // convert string to float data
         temp = received.toFloat();
 
+        // Piezo function, ring the piezo buzzer if temp > tempLimit
         if (temp > tempLimit)
         {
             PiezoSound();
         }
         else
         {
+            // cut of from the buzzer
             digitalWrite(buzzerPin, HIGH);
         }
     }
 }
 
+// Piezo ring sound
 void PiezoSound()
 {
     tone(buzzerPin, 1000); // Send 1kHz tone to buzzer
--- nodeMCU-transmitter.cpp
+++ nodeMCU-transmitter.cpp
@@ -1,24 +1,24 @@
 #include <SoftwareSerial.h>
 #include <DHT.h>
 
-#define DHTPIN 14
-#define DHTTYPE DHT11
+#define DHTPIN 14     // D5 PIN
+#define DHTTYPE DHT11 // DHT11 type sensor
 
 SoftwareSerial BTSerial(5, 4); // RX | TX
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
-    Serial.begin(9600);
-    dht.begin();
-    BTSerial.begin(38400); // Set HC-05 communication speed to 9600
-    Serial.println("Master Bluetooth Device is Ready to Pair");
+    Serial.begin(9600);                                         // set up Serial baud 9600
+    dht.begin();                                                // function to call dht sensor
+    BTSerial.begin(38400);                                      // Set HC-05 communication speed to 38400
+    Serial.println("Master Bluetooth Device is Ready to Pair"); // print to tag the device as the master
 }
 
 void loop()
 {
-    // reading temperature from DHT Sensor
+    //  reading temperature from DHT Sensor
     float t = dht.readTemperature();
-    BTSerial.println(t);
-    delay(100); // Add a delay to avoid spamming messages
+    BTSerial.println(t); // send the sensor data to BtSerial
+    delay(100);          // Add adelay to avoid spamming messages
 }
